d93015ec3260beaa17d57030cab52447d427c2c5
棠筱柒 <54069236+tangxiaoqi-tangxiao@users.noreply.github.com>
2025-06-13 22:17:17 +0800

    更新 扩展版本

--- wxt.config.ts
+++ wxt.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     name: '__MSG_appName__',
     description: '__MSG_appDesc__',
     default_locale: 'zh_CN',
-    version: "1.0.19",
+    version: "1.0.20",
     permissions: ['storage',"bookmarks","favicon"],
     icons: {
       16: 'icon/favicon_16x16.png',
