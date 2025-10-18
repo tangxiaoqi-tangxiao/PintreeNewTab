88a36fdbf8f4a5b9522a790e42d6342655c88996
tangxiaoqi <1726567547@qq.com>
2025-10-18 18:28:09 +0800

    Changed：version: "1.0.22"

--- wxt.config.ts
+++ wxt.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     name: '__MSG_appName__',
     description: '__MSG_appDesc__',
     default_locale: 'zh_CN',
-    version: "1.0.21",
+    version: "1.0.22",
     permissions: ['storage',"bookmarks","favicon"],
     icons: {
       16: 'icon/favicon_16x16.png',
